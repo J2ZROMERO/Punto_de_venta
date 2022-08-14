2d0a51269d4e3e776110966236c56cfe2099e200
Jhosep <jose-zromero@hotmail.com>
2022-08-13 22:27:44 -0500

    codigos/ interaccion con DB generado

--- com.pvCsegunda.v1/src/interfaces/Bar_Code.java
+++ com.pvCsegunda.v1/src/interfaces/Bar_Code.java
@@ -14,31 +14,51 @@ import javax.swing.SwingConstants;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
+
+import org.icepdf.ri.common.ComponentKeyBinding;
+import org.icepdf.ri.common.SwingController;
+import org.icepdf.ri.common.SwingViewBuilder;
+
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.Barcode;
 import com.itextpdf.text.pdf.Barcode128;
 import com.itextpdf.text.pdf.Barcode39;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import conexionDB.DB_productos;
+import conexionDB.DB_ventas;
 import jbarcodebean.JBarcodeBean;
 import net.sourceforge.jbarcodebean.model.Interleaved25;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Bar_Code extends JFrame {
-
+	private List<String> letras = new ArrayList<>();
 	private JPanel contentPane;
 	public JTextField txt_id;
 	private JTable tbl_bar_code;
@@ -87,13 +107,21 @@ public class Bar_Code extends JFrame {
 		
 	}
 	
+	
+	
+	
+	
+	
+	
+	
+	
 	public void Generar_Codigo() throws DocumentException {
 		try {
 			//se crea el documento pdf
 		    Document doc=new Document();
 		    //ruta de guardado del documento
 		    //PdfWriter pdf=PdfWriter.getInstance(doc,new FileOutputStream("D:/PDF/codigo.pdf"));
-		    PdfWriter pdf=PdfWriter.getInstance(doc,new FileOutputStream("E:/RESPALDO/Punto_de_venta/com.pvCsegunda.v1/src/Codigo_De_Barras/codigo.pdf"));
+		    PdfWriter pdf=PdfWriter.getInstance(doc,new FileOutputStream("E:/Users/windows/Desktop/Nueva carpeta/codigo.pdf"));
 		    
 		    //se abre el documento
 		    doc.open();
@@ -111,7 +139,7 @@ public class Bar_Code extends JFrame {
 		    Image img = code.createImageWithBarcode(pdf.getDirectContent(), BaseColor.BLACK, BaseColor.BLACK);
 		    //se le puede dar un tamaño al condigo de barras
 		    img.scalePercent(120);
-		    
+		    img.setAbsolutePosition(10, 600);
 		    //insercion de la imagen con la posicion
 		    img.setAlignment(Chunk.ALIGN_CENTER);
 		    
@@ -139,14 +167,14 @@ public class Bar_Code extends JFrame {
 	
 	public Bar_Code() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 636, 598);
+		setBounds(100, 100, 957, 712);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 620, 559);
+		panel.setBounds(0, 0, 941, 673);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -169,59 +197,55 @@ public class Bar_Code extends JFrame {
 		JLabel lbl_id = new JLabel("ID");
 		lbl_id.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl_id.setFont(new Font("Dialog", Font.BOLD, 13));
-		lbl_id.setBounds(10, 70, 69, 20);
+		lbl_id.setBounds(83, 42, 69, 20);
 		panel.add(lbl_id);
 		
 		txt_id = new JTextField();
 		txt_id.setFont(new Font("Dialog", Font.BOLD, 12));
-		txt_id.setBounds(89, 70, 108, 20);
+		txt_id.setBounds(162, 42, 180, 20);
+		
+		
 		panel.add(txt_id);
 		
-		txt_id.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-				int key = e.getKeyChar();
-
-			    boolean numeros = key >= 48 && key <= 57;
-			    
-			    if (!numeros)
-			    {
-			        e.consume();
-			    }
-			    /*if (txt_id.getText().trim().length() == 10) {
-			        e.consume();
-			    }*/
-			}
-		});
+//		txt_id.addKeyListener(new KeyAdapter() {
+//			@Override
+//			public void keyTyped(KeyEvent e) {
+//				int key = e.getKeyChar();
+//
+//			    boolean numeros = key >= 48 && key <= 57;
+//			    
+//			    if (!numeros)
+//			    {
+//			        e.consume();
+//			    }
+//			    /*if (txt_id.getText().trim().length() == 10) {
+//			        e.consume();
+//			    }*/
+//			}
+//		});
 		txt_id.setColumns(10);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(237, 39, 373, 96);
+		scrollPane.setBounds(370, 39, 571, 55);
 		panel.add(scrollPane);
 		
 		tbl_bar_code = new JTable();
-		tbl_bar_code.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"<html><center>PRODUCTO</center></html>", "<html><center>DISTINTIVO 1</center></html>", "<html><center>DISTINTIVO 2</center></html>"
-			}
-		));
+		
 		scrollPane.setViewportView(tbl_bar_code);
 		
 		JLabel lbl_id_1 = new JLabel("<html><center>CANTIDAD DE ETIQUETAS</center></html>");
 		lbl_id_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl_id_1.setFont(new Font("Dialog", Font.BOLD, 13));
-		lbl_id_1.setBounds(10, 146, 147, 35);
+		lbl_id_1.setBounds(10, 115, 147, 35);
 		panel.add(lbl_id_1);
 		
 		txt_cantidad = new JTextField();
 		txt_cantidad.setEditable(false);
 		txt_cantidad.setText("0");
-		txt_cantidad.setHorizontalAlignment(SwingConstants.CENTER);
+		txt_cantidad.setHorizontalAlignment(SwingConstants.RIGHT);
 		txt_cantidad.setFont(new Font("Dialog", Font.BOLD, 14));
 		txt_cantidad.setColumns(10);
-		txt_cantidad.setBounds(10, 190, 44, 35);
+		txt_cantidad.setBounds(162, 115, 72, 35);
 		
 		txt_cantidad.addKeyListener(new KeyAdapter() {
 			@Override
@@ -241,7 +265,7 @@ public class Bar_Code extends JFrame {
 		
 		JButton btn_mas = new JButton("+");
 		btn_mas.setFont(new Font("Dialog", Font.BOLD, 15));
-		btn_mas.setBounds(64, 190, 44, 35);
+		btn_mas.setBounds(244, 115, 44, 35);
 		
 		btn_mas.addMouseListener(new MouseAdapter() {
 			@Override
@@ -274,67 +298,145 @@ public class Bar_Code extends JFrame {
 			}
 		});
 		btn_menos.setFont(new Font("Dialog", Font.BOLD, 22));
-		btn_menos.setBounds(118, 190, 44, 35);
+		btn_menos.setBounds(298, 115, 44, 35);
 		
 		panel.add(btn_menos);
 		
-		JLabel lbl_añadir_precio_a_etiquetas = new JLabel("<html><center>AÑADIR PRECIO A ETIQUETAS</center></html>");
-		lbl_añadir_precio_a_etiquetas.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_añadir_precio_a_etiquetas.setFont(new Font("Dialog", Font.BOLD, 13));
-		lbl_añadir_precio_a_etiquetas.setBounds(208, 146, 153, 35);
-		panel.add(lbl_añadir_precio_a_etiquetas);
-		
-		JButton btn_encendido = new JButton("ON");
-		btn_encendido.setFont(new Font("Dialog", Font.BOLD, 13));
-		btn_encendido.setBounds(218, 197, 61, 23);
-		
-		btn_encendido.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				frame.requestFocus();
-			}
-		});
-		panel.add(btn_encendido);
-		
-		JButton btn_apagado = new JButton("OFF");
-		btn_apagado.setFont(new Font("Dialog", Font.BOLD, 13));
-		btn_apagado.setBounds(289, 197, 61, 23);
-		
-		btn_apagado.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				frame.requestFocus();
-			}
-		});
-		panel.add(btn_apagado);
-		
 		JButton btn_ejecutar = new JButton("EJECUTAR");
-		btn_ejecutar.setFont(new Font("Dialog", Font.BOLD, 13));
-		btn_ejecutar.setBounds(34, 268, 122, 23);
 		
-		btn_ejecutar.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				JOptionPane.showMessageDialog(null,"AÑADIENDO ELEMENTOS A LA HOJA");
-				
-				lbl_cantidad.setText(txt_cantidad.getText());
-				
-				try {
-					Generar_Img_Barras();
-					Generar_Codigo();
-					txt_id.setText("");
-					txt_cantidad.setText("0");
-					txt_id.requestFocus();
-				} catch (DocumentException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
+		btn_ejecutar.setFont(new Font("Dialog", Font.BOLD, 13));
+		btn_ejecutar.setBounds(162, 161, 180, 35);
+//		
+//		btn_ejecutar.addMouseListener(new MouseAdapter() {
+//			public void mouseClicked(MouseEvent e) {
+//				JOptionPane.showMessageDialog(null,"AÑADIENDO ELEMENTOS A LA HOJA");
+//				
+//				lbl_cantidad.setText(txt_cantidad.getText());
+//				
+//				try {
+//					Generar_Img_Barras();
+//					Generar_Codigo();
+//					txt_id.setText("");
+//					txt_cantidad.setText("0");
+//					txt_id.requestFocus();
+//				} catch (DocumentException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//			}
+//		});
 		panel.add(btn_ejecutar);
 		
 		JButton btn_limpiar_hoja = new JButton("LIMPIAR HOJA");
+		btn_limpiar_hoja.setHorizontalAlignment(SwingConstants.LEFT);
+		btn_limpiar_hoja.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btn_limpiar_hoja.setFont(new Font("Dialog", Font.BOLD, 13));
-		btn_limpiar_hoja.setBounds(192, 268, 141, 23);
+		btn_limpiar_hoja.setBounds(370, 173, 142, 23);
+	
+		panel.add(btn_limpiar_hoja);
+		
+		JButton btn_buscar = new JButton("BUSCAR");
+		btn_buscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btn_buscar.setFont(new Font("Dialog", Font.BOLD, 13));
+		btn_buscar.setBounds(162, 73, 180, 31);
+		
+		btn_buscar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+//			
+				
+				try {
+				tbl_bar_code.setModel(DB_productos.model_view_prod_barcode(Long.parseLong( txt_id.getText())));
+				} catch (NumberFormatException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+//				if (!txt_id.getText().equals("")) {
+//					JOptionPane.showMessageDialog(null,"LLENANDO CAMPOS");
+//					txt_id.setText("");
+//					txt_cantidad.setText("");
+//					txt_id.requestFocus();
+//				}
+//				else {
+//					Bar_Code_Buscar bcb = new Bar_Code_Buscar();
+//					bcb.setVisible(true);
+//					bcb.setFocusable(true);
+//					bcb.setLocationRelativeTo(null);
+//				}
+			}
+		});
+		panel.add(btn_buscar);
+		
+		JButton btn_imprimir = new JButton("IMPRIMIR");
+		btn_imprimir.setHorizontalAlignment(SwingConstants.LEFT);
+		btn_imprimir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String url = "E:/Users/windows/Desktop/Punto_de_venta/com.pvCsegunda.v1/bin/Codigo_De_Barras/codigo.pdf";
+				File archivo = new File(url);
+				
+				if(archivo.exists()) {
+			
+					if(Desktop.isDesktopSupported()) {
+						try {
+							Desktop.getDesktop().open(archivo);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}else{
+						System.out.println("no soportado");
+					}
+					
+				}else{
+				System.out.println("no existe archivo");
+			}
+			}
+		});
+		btn_imprimir.setFont(new Font("Dialog", Font.BOLD, 13));
+		btn_imprimir.setBounds(370, 139, 142, 23);
+		panel.add(btn_imprimir);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(10, 222, 931, 451);
+		panel.add(scrollPane_1);
+		
+		btn_ejecutar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		
+				try {
+		
+					if(txt_id.getText().equalsIgnoreCase("") && txt_cantidad.getText().equalsIgnoreCase("") || !txt_id.getText().equalsIgnoreCase("") && txt_cantidad.getText().equalsIgnoreCase("0")) {
+						System.out.println("nothing to generate");
+					}else {
+
+						createPdf(txt_id.getText(), Integer.parseInt( txt_cantidad.getText()));
+						openpdf(scrollPane_1 );	
+					}
+					
+					
+			
+		} catch (NumberFormatException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (DocumentException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+			}
+		});
 		
 		btn_limpiar_hoja.addMouseListener(new MouseAdapter() {
 			@Override
@@ -343,60 +445,30 @@ public class Bar_Code extends JFrame {
 				int resp = JOptionPane.showConfirmDialog(null, "¿ESTAS SEGURO DE LIMPIAR LA HOJA?", "¡ALERTA!", JOptionPane.YES_NO_OPTION);
 				
 				if(resp == 0) {
+				
+					try {
+						createPdf("", 0);
+						letras.clear();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (DocumentException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					openpdf(scrollPane_1);	
 				JOptionPane.showMessageDialog(null,"LIMPIANDO HOJA");
-				lbl_bar_code.setIcon(null);
-				lbl_cantidad.setText("");
-				lbl_precio.setText("");
+				//lbl_bar_code.setIcon(null);
+				//lbl_cantidad.setText("");
+				//lbl_precio.setText("");
 				txt_id.setText("");
 				txt_id.requestFocus();
 				}
 			}
 		});
-		panel.add(btn_limpiar_hoja);
-		
-		JButton btn_buscar = new JButton("BUSCAR");
-		btn_buscar.setFont(new Font("Dialog", Font.BOLD, 13));
-		btn_buscar.setBounds(402, 146, 180, 23);
-		
-		btn_buscar.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				if (!txt_id.getText().equals("")) {
-					JOptionPane.showMessageDialog(null,"LLENANDO CAMPOS");
-					txt_id.setText("");
-					txt_cantidad.setText("");
-					txt_id.requestFocus();
-				}
-				else {
-					Bar_Code_Buscar bcb = new Bar_Code_Buscar();
-					bcb.setVisible(true);
-					bcb.setFocusable(true);
-					bcb.setLocationRelativeTo(null);
-				}
-			}
-		});
-		panel.add(btn_buscar);
-		
-		JButton btn_guardar_imagen = new JButton("GUARDAR IMAGEN");
-		btn_guardar_imagen.setFont(new Font("Dialog", Font.BOLD, 13));
-		btn_guardar_imagen.setBounds(402, 190, 180, 23);
-		panel.add(btn_guardar_imagen);
-		
-		JButton btn_imprimir = new JButton("IMPRIMIR");
-		btn_imprimir.setFont(new Font("Dialog", Font.BOLD, 13));
-		btn_imprimir.setBounds(402, 234, 180, 23);
-		panel.add(btn_imprimir);
-		
-		JButton btn_guardar_pdf = new JButton("GUARDAR PDF");
-		btn_guardar_pdf.setFont(new Font("Dialog", Font.BOLD, 13));
-		btn_guardar_pdf.setBounds(402, 279, 180, 23);
-		panel.add(btn_guardar_pdf);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(10, 318, 600, 230);
-		panel.add(scrollPane_1);
-		
+		/*
+		 
+		 panel con recuadro centrico resaltando precio titulo
 		JPanel pnl_vista = new JPanel();
 		scrollPane_1.setViewportView(pnl_vista);
 		pnl_vista.setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -430,6 +502,166 @@ public class Bar_Code extends JFrame {
 		lbl_precio.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl_precio.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lbl_precio.setBounds(301, 176, 113, 28);
-		pnl_vista.add(lbl_precio);
+		pnl_vista.add(lbl_precio);*/
+		envento_barcode eventos = new envento_barcode();
+		eventos.evento_barcodes(tbl_bar_code, txt_id);
+		txt_id.addKeyListener(eventos);
+		
 	}
+	public void createPdf(String id,int cantidad) throws IOException, DocumentException {
+		Document document = new Document();
+	String url = "E:/Users/windows/Desktop/Punto_de_venta/com.pvCsegunda.v1/bin/Codigo_De_Barras/codigo.pdf";
+	PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(url));
+System.out.println(id+cantidad);
+		if(cantidad == 0) {
+
+	document.open();
+	document.add(new  Chunk(""));
+	document.close();		
+		}else {
+
+	document.open();
+	PdfPTable table = new PdfPTable(4);
+
+	table.setWidthPercentage(100);
+	table.setHorizontalAlignment(0);
+
+
+
+	for(int i = 0; i < cantidad ; i++) {
+		letras.add(i, id);
+		
+	}
+
+	while((cantidad % 4) != 0) {
+	cantidad++;
+	letras.add("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+	};
+		
+
+
+
+
+
+
+	for (int i = 0; i <= letras.size()-1; i++) {
+		
+	    table.addCell(createBarcode(writer, letras.get(i)));
+	}
+
+	document.add(table);
+	document.close();		
+		}
+
+	}
+
+		public static PdfPCell createBarcode(PdfWriter writer, String code) throws DocumentException, IOException {
+		Barcode128 barcode = new Barcode128();
+		barcode.setCodeType(Barcode.CODE128);
+
+
+		barcode.setCode(code);
+
+
+
+		PdfPCell cell = new PdfPCell(barcode.createImageWithBarcode(writer.getDirectContent(), BaseColor.BLACK, BaseColor.GRAY), true);
+		cell.setPadding(10);
+		return cell;
+
+		}
+		
+		
+public	  void openpdf(JScrollPane panel){
+	  
+	String url = "E:/Users/windows/Desktop/Punto_de_venta/com.pvCsegunda.v1/bin/Codigo_De_Barras/codigo.pdf";
+    try {
+           SwingController control=new SwingController();
+            SwingViewBuilder factry=new SwingViewBuilder(control);
+            JPanel veiwerCompntpnl=factry.buildViewerPanel();
+            ComponentKeyBinding.install(control, veiwerCompntpnl);
+            
+           control.getDocumentViewController();
+            control.getDocumentViewController().setAnnotationCallback(new org.icepdf.ri.common.MyAnnotationCallback( control.getDocumentViewController()));
+           
+            control.openDocument(url);
+        panel.setViewportView(veiwerCompntpnl); 
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this,"Cannot Load Pdf");
+        }
 }
+}
+
+class envento_barcode implements KeyListener{
+
+	private JTable tabla;
+	private JTextField campo;
+	
+	public envento_barcode() {
+		
+	}
+	
+
+	 void evento_barcodes(JTable tabla, JTextField campo) {
+		// TODO Auto-generated method stub
+this.tabla = tabla;
+		this.campo = campo;
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		int key = e.getKeyChar();
+		boolean numeros = key >= 48 && key <=57;
+		if(!numeros) {
+			
+		e.consume();	
+		}
+			}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	
+
+
+						
+				}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		
+if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+	
+	
+	if(campo.getText().equalsIgnoreCase("")) {
+		System.out.println("vaacio");
+	}else {
+		
+		
+try {
+tabla.setModel(	DB_productos.model_view_prod_barcode(Long.parseLong(campo.getText() )));
+} catch (NumberFormatException e1) {
+	// TODO Auto-generated catch block
+	e1.printStackTrace();
+} catch (SQLException e1) {
+	// TODO Auto-generated catch block
+	e1.printStackTrace();
+}
+		
+					
+					 
+	
+}
+		
+}}
+	
+
+	
+}
+
